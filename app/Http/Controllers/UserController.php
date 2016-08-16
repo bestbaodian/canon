6df32e12e268e6e56589dbc630a817f4c19b9012
王浩东 <2691246603@qq.com>
2016-08-16 21:44:39 +0800

    whd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -103,7 +103,7 @@ class UserController extends Controller
                  */
                 $da  = DB::table('users')->select("user_filedir")->where("user_id",$uid)->first();
                 Session::put('user_filedir',$da['user_filedir']);
-                header('location:user/setavator');
+                return redirect("user/setavator");
             }
         }
     }
