a08468df4a508950187531b9a9fb9ebfcadf6e5f
mttstudy <1412486710@qq.com>
2016-08-22 21:12:43 +0800

    trr

--- resources/views/company/college_exam.blade.php
+++ resources/views/company/college_exam.blade.php
@@ -1,8 +1,9 @@
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <title>
-        公司试题
+        公司试题------面试宝典
     </title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
     <meta name="renderer" content="webkit">
@@ -16,34 +17,113 @@
     <link rel="stylesheet" href="css/d79d81e9ab144c28aae8b073106e6881.css" type="text/css" />
     <link rel="stylesheet" href="css/nowcoder-ui.css">
 </head>
+<style>
+    .xxx{
+        padding-left:10px;
+    }
+    .zzz{
+        padding-left:10px;
+    }
+
+</style>
 @extends('layouts.master')
 @section('sidebar')
-@parent
+    @parent
 
-<body>
-	   <div style="width:72%; height:80%; background:#fff; border:1px solid #ccc; margin-left:15%; margin-top:10px; margin-bottom:5px;">
+    <body style="background:#fff;">
+    <input type="hidden"  id="college">
+    <div class="nk-main test-center-page clearfix" style=" margin-top:50px;">
+        <div class="test-center-bar">
+            <ul class="test-center-menu">
+
+                <li>
+                    <div class="tcm-mod">
+                        <div class="tcm-arrow"></div>
+                        <h4 class="tcm-hd">简历</h4>
+                        <div class="tcm-bd">
+                            <a href="{{url('company')}}" class="xxx" data-id="134">简历范文</a>
+                        </div>
+                    </div>
+
+                </li>
+            </ul>
+
+        </div>
+        <div class="nk-content">
 
 
+            <div class="module-box">
+                <div class="menu-box">
+                    <ul class="menu clearfix">
+                        <li class="selected"><a href="javascript:void(0);">简历</a></li>
+                    </ul>
+                </div>
+                <div class="module-body" id="exam">
+                    <center>
+                        <h4>{{$arr['g_name']}}</h4>
+                        <img src="{{$arr['g_dir']}}"  /><br/>
+                        <input type="submit" style="width: 400px;height: 50px;font-weight: bold;font-size: 20px;color: #FF0000;font-family: 微软雅黑;" value="【立即下载】点击本按钮直接下载简历" onclick="window.open('http://jianli.yjbys.com/{{$arr['g_down']}}')">                    </center>
+                </div>
+                <div class="pagination">
+                    <style>
+                        .pager li{
+                            float:left;
+                            margin-left:200px;
+                        }
+                    </style>
 
-
-		<div style="width:975px; height:465px; margin-top:20px;">
-           <span><?php echo $arr['g_name'] ?></span>
-            <div>
-                <img src="<?php echo $arr['g_dir'] ?>" width="350px" height="450px"/>
+                </div>
             </div>
-       </div>
-		</div>
-	   </div>
+        </div>
+    </div>
 
-	   <script src="js/jquery-1.8.3.min.js"></script>
-	   <script>
-	   $(function(){
-	   	$("#sel_hide").hide();
-	   })
-	   $(document).on("click","#img",function(){
-	   	$("#sel_hide").show();
-	   })
-	   </script>
-</body>
-    @endsection
+
+    <div class="ad-window-sm js-global-tips" style="display:none;">
+        <a href="javascript:void(0);" class="ad-close">X</a>
+        <div class="ad-live-active"></div>
+        <a class="link-green js-global-tips-href" target="_blank" href="">点击查看>></a>
+    </div>
+
+    </div>
+    <script src="js/sea_1.js" type="text/javascript"></script>
+    <script src="js/base.js"></script>
+    <script src="js/jquery-1.8.3.min.js"></script>
+
+    <script>
+        $(document).on("click",".zzz",function(){
+            var name = $(this).attr("data-id");
+            $("#college").attr('value',name);
+            $.get("college",{name:name},function(data){
+                $("#exam").html(data);
+            })
+        })
+        $(document).on("click",".xxx",function(){
+            var name_z = $("#college").val();
+            var name_x = $(this).attr("data-id");
+            $.get("college_x",{name_z:name_z,name_x:name_x},function(data){
+                $("#exam").html(data);
+            })
+        })
+
+    </script>
+
+
+
+
+
+
+    <script type="text/javascript">
+        seajs.use('nowcoder/1.2.456/javascripts/site/common/index');
+        seajs.use('nowcoder/1.2.456/javascripts/site/common/nav');
+    </script>
+    <span id='cnzz_stat_icon_1253353781' style="display:none;"></span>
+    <script type="text/javascript">
+        window.parameter = {
+            mutiTagIds: ''
+        };
+        seajs.use('nowcoder/1.2.456/javascripts/site/contest/paperList');
+    </script>
+    </body>
+@endsection
 </html>
+
--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -33,56 +33,22 @@
 <body style="background:#fff;">
 <input type="hidden"  id="college">
 <div class="nk-main test-center-page clearfix" style=" margin-top:50px;">
-<div class="test-center-bar">
-<ul class="test-center-menu">
-<li>
-<div class="tcm-mod">
-<div class="tcm-arrow"></div>
-<h4 class="tcm-hd">职业方向</h4>
-<div class="tcm-bd">
-<a class="zzz" data-id="互联网工程" href="#">互联网工程</a>
-<a class="zzz" data-id="web前端" href="#">web前端</a>
-</div>
-</div>
-<div class="mod-sub-wrap">
-<div class="tcm-sub-mod">
-<h4 class="tcm-hd">职业方向</h4>
-<div class="tcm-bd">
-@foreach($re as $key=>$value)
-<a class="zzz" data-id="{{$value['d_name']}}" href="#">{{$value['d_name']}}</a>
-@endforeach
-</div>
-</div>
-</div>
-</li>
-<li>
-<div class="tcm-mod">
-<div class="tcm-arrow"></div>
-<h4 class="tcm-hd">公司</h4>
-<div class="tcm-bd">
-<a href="#" class="xxx"
-data-id="134">阿里巴巴</a>
-<a href="#" class="xxx" data-id="腾讯">腾讯</a>
-<a href="#" class="xxx" data-id="百度">百度</a>
-</div>
-</div>
-<div class="mod-sub-wrap">
-<div class="tcm-sub-mod">
-<h4 class="tcm-hd">公司</h4>
-<div class="tcm-bd">
-@foreach($arr as $key=>$value)
-<a class="xxx" data-id="{{$value['company_name']}}" href="#">{{$value['company_name']}}</a>
+    <div class="test-center-bar">
+        <ul class="test-center-menu">
 
-@endforeach
-</div>
-</div>
-</div>
-</li>
-</ul>
-<div class="module-box">
-<a href="#" class="btn btn-primary btn-block">2016春招备考计划</a>
-</div>
-</div>
+            <li>
+                <div class="tcm-mod">
+                    <div class="tcm-arrow"></div>
+                    <h4 class="tcm-hd">简历</h4>
+                    <div class="tcm-bd">
+                        <a href="{{url('company')}}" class="xxx" data-id="134">简历范文</a>
+                    </div>
+                </div>
+
+            </li>
+        </ul>
+
+    </div>
 <div class="nk-content">
 
 
@@ -102,13 +68,23 @@ data-id="134">阿里巴巴</a>
 <div class="web-logoimg">
 <img src="{{$value['g_dir']}}" style="width:50px; height:50px;" />
 </div>
-<div class="exam-foot">已有{{$value['g_click']}}人参加</div>
+    <?php
+    if($value['g_click']==0){
+    ?>
+    <div class="exam-foot">还没有人观看</div>
+    <?php
+    }else{
+    ?>
+<div class="exam-foot">已有{{$value['g_click']}}人观看</div>
+    <?php
+    }
+    ?>
 <dl class="exam-info">
 <dd><span class="link-green"></span></dd>
 <dd class="exam-btn"><span class="btn  btn-block btn-primary" >查看详情</span></dd>
 </dl>
-</div>                                               
-</a> 
+</div>
+</a>
 </li>
 @endforeach
 </ul>
