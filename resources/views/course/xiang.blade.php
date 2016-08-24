55d35d5494533deedaecf3d111663394f4723188
王浩东 <2691246603@qq.com>
2016-08-25 07:25:36 +0800

    whd

--- resources/views/course/xiang.blade.php
+++ resources/views/course/xiang.blade.php
@@ -26,60 +26,6 @@
         #star p{position:absolute;top:20px;width:159px;height:60px;display:none;background:url(images/icon.gif ) no-repeat;padding:7px 10px 0;}
         #star p em{color:#f60;display:block;font-style:normal;}
     </style>
-    <script type="text/javascript">
-        window.onload = function ()
-        {
-            var oStar = document.getElementById("star");
-            var aLi = oStar.getElementsByTagName("li");
-            var oUl = oStar.getElementsByTagName("ul")[0];
-            var oSpan = oStar.getElementsByTagName("span")[1];
-            var oP = oStar.getElementsByTagName("p")[0];
-            var i = iScore = iStar = 0;
-            var aMsg = [
-                "很不满意|差得太离谱，面试根本不问这个问题",
-                "不满意|内容不实用，去了公司用不到，不满意",
-                "一般|内容一般，逻辑描述不清晰",
-                "满意|逻辑挺好，内容晦涩了点，还是挺满意的",
-                "非常满意|各方面都非常好，非常满意"
-            ]
-            for (i = 1; i <= aLi.length; i++)
-            {
-                aLi[i - 1].index = i;
-                //鼠标移过显示分数
-                aLi[i - 1].onmouseover = function ()
-                {
-                    fnPoint(this.index);
-                    //浮动层显示
-                    oP.style.display = "block";
-                    //计算浮动层位置
-                    oP.style.left = oUl.offsetLeft + this.index * this.offsetWidth - 104 + "px";
-                    //匹配浮动层文字内容
-                    oP.innerHTML = "<em><b>" + this.index + "</b> 分 " + aMsg[this.index - 1].match(/(.+)\|/)[1] + "</em>" + aMsg[this.index - 1].match(/\|(.+)/)[1]
-                };
-                //鼠标离开后恢复上次评分
-                aLi[i - 1].onmouseout = function ()
-                {
-                    fnPoint();
-                    //关闭浮动层
-                    oP.style.display = "none"
-                };
-                //点击后进行评分处理
-                aLi[i - 1].onclick = function ()
-                {
-                    iStar = this.index;
-                    oP.style.display = "none";
-                    oSpan.innerHTML = "<strong>" + (this.index) + " 分</strong> (" + aMsg[this.index - 1].match(/\|(.+)/)[1] + ")"
-                }
-            }
-            //评分处理
-            function fnPoint(iArg)
-            {
-                //分数赋值
-                iScore = iArg || iStar;
-                for (i = 0; i < aLi.length; i++) aLi[i].className = i < iScore ? "on" : "";
-            }
-        };
-    </script>
 <body>
 
 @extends('layouts.master')
@@ -292,7 +238,7 @@ $l=isset($_GET['l'])?$_GET['l']:0;
                 <p class="person_num"><em>{{$synthesize["b"]}}</em>位同学参与评价</p>
             </div><!--evaluation-info end-->
             <div class="evaluation">
-                <?php if(Session::get('id')==""){ ?>
+                <?php if(Session::get('id')!=""){ ?>
                 <?php foreach($ping as $k=>$v){?>
                 <div class="evaluation-con" id="list">
                     <div class="content-box">
@@ -456,22 +402,9 @@ $l=isset($_GET['l'])?$_GET['l']:0;
 </div>
 
 @endsection
-<div id="J_GotoTop" class="elevator">
-    <a class="elevator-weixin" href="javascript:;">
-        <div class="elevator-weixin-box">
-        </div>
-    </a>
-    <a class="elevator-msg" href="http://www.imooc.com/user/feedback" target="_blank" id="feedBack"></a>
-    <a class="elevator-app" href="http://www.imooc.com/mobile/app">
-        <div class="elevator-app-box">
-        </div>
-    </a>
-    <a class="elevator-top" href="javascript:;" style="display: none;" id="backTop"></a>
-</div>
 
 
 <!--script-->
-<script src="js/ssologin.js"></script>
 
 <script type="text/javascript">
 </script>
